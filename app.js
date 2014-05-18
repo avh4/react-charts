4102cb092e94601734fbfb9805f2af9a23ef1333
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-18 16:32:41 -0700

    Fix chart position and clean up top-level SVG

--- app.js
+++ app.js
@@ -51,49 +51,46 @@ var App = React.createClass({
     }
 
     return <svg viewBox="0 0 500 500">
-  <g className="nvd3 nv-wrap nv-scatterChart
-    nv-chart-92514" transform="translate(75,30)">
+  <g className="nvd3 nv-wrap nv-scatterChart" transform="translate(0,30)">
+    <XAxis/>
+    <YAxis/>
     <g>
-      <XAxis/>
-      <YAxis/>
-      <g>
-        <Series className="chart-series s0" data={data[0].values} coord={coord}/>
-        <Series className="chart-series s1" data={data[1].values} coord={coord}/>
-        <Series className="chart-series s2" data={data[2].values} coord={coord}/>
-        <Series className="chart-series s3" data={data[3].values} coord={coord}/>
-      </g>
-      <g transform="translate(0,420)">
-        <Distribution className="s0" y1="0" y2="8" data={data[0].values} coord={coord}/>
-        <Distribution className="s1" y1="0" y2="8" data={data[1].values} coord={coord}/>
-        <Distribution className="s2" y1="0" y2="8" data={data[2].values} coord={coord}/>
-        <Distribution className="s3" y1="0" y2="8" data={data[3].values} coord={coord}/>
-      </g>
-      <g transform="translate(-8,0)">
-        <Distribution className="s0" x1="0" x2="8" data={data[0].values} coord={coord}/>
-        <Distribution className="s1" x1="0" x2="8" data={data[1].values} coord={coord}/>
-        <Distribution className="s2" x1="0" x2="8" data={data[2].values} coord={coord}/>
-        <Distribution className="s3" x1="0" x2="8" data={data[3].values} coord={coord}/>
-      </g>
-      <g className="nv-legendWrap"
-        transform="translate(0,-30)"><g className="nvd3 nv-legend"
-        transform="translate(0,5)"><g transform="translate(317,5)"><g
-        className="nv-series" transform="translate(0,5)"><circle
-        className="nv-legend-symbol nv-series-0" r="5"></circle><text
-        text-anchor="start" className="nv-legend-text" dy=".32em"
-        dx="8">Group 0</text></g><g className="nv-series"
-        transform="translate(72,5)"><circle className="nv-legend-symbol nv-series-1"
-        r="5"></circle><text text-anchor="start"
-        className="nv-legend-text" dy=".32em" dx="8">Group 1</text></g><g
-        className="nv-series" transform="translate(144,5)"><circle
-        className="nv-legend-symbol nv-series-2" r="5"></circle><text
-        text-anchor="start" className="nv-legend-text" dy=".32em"
-        dx="8">Group 2</text></g><g className="nv-series"
-        transform="translate(216,5)"><circle className="nv-legend-symbol nv-series-3"
-        r="5"></circle><text text-anchor="start"
-        className="nv-legend-text" dy=".32em" dx="8">Group
-      3</text></g></g></g></g><g
-        className="nv-controlsWrap"></g>
+      <Series className="chart-series s0" data={data[0].values} coord={coord}/>
+      <Series className="chart-series s1" data={data[1].values} coord={coord}/>
+      <Series className="chart-series s2" data={data[2].values} coord={coord}/>
+      <Series className="chart-series s3" data={data[3].values} coord={coord}/>
     </g>
+    <g transform="translate(0,420)">
+      <Distribution className="s0" y1="0" y2="8" data={data[0].values} coord={coord}/>
+      <Distribution className="s1" y1="0" y2="8" data={data[1].values} coord={coord}/>
+      <Distribution className="s2" y1="0" y2="8" data={data[2].values} coord={coord}/>
+      <Distribution className="s3" y1="0" y2="8" data={data[3].values} coord={coord}/>
+    </g>
+    <g transform="translate(-8,0)">
+      <Distribution className="s0" x1="0" x2="8" data={data[0].values} coord={coord}/>
+      <Distribution className="s1" x1="0" x2="8" data={data[1].values} coord={coord}/>
+      <Distribution className="s2" x1="0" x2="8" data={data[2].values} coord={coord}/>
+      <Distribution className="s3" x1="0" x2="8" data={data[3].values} coord={coord}/>
+    </g>
+    <g className="nv-legendWrap"
+      transform="translate(0,-30)"><g className="nvd3 nv-legend"
+      transform="translate(0,5)"><g transform="translate(317,5)"><g
+      className="nv-series" transform="translate(0,5)"><circle
+      className="nv-legend-symbol nv-series-0" r="5"></circle><text
+      text-anchor="start" className="nv-legend-text" dy=".32em"
+      dx="8">Group 0</text></g><g className="nv-series"
+      transform="translate(72,5)"><circle className="nv-legend-symbol nv-series-1"
+      r="5"></circle><text text-anchor="start"
+      className="nv-legend-text" dy=".32em" dx="8">Group 1</text></g><g
+      className="nv-series" transform="translate(144,5)"><circle
+      className="nv-legend-symbol nv-series-2" r="5"></circle><text
+      text-anchor="start" className="nv-legend-text" dy=".32em"
+      dx="8">Group 2</text></g><g className="nv-series"
+      transform="translate(216,5)"><circle className="nv-legend-symbol nv-series-3"
+      r="5"></circle><text text-anchor="start"
+      className="nv-legend-text" dy=".32em" dx="8">Group
+    3</text></g></g></g></g><g
+      className="nv-controlsWrap"></g>
   </g>
 </svg>;
   }
