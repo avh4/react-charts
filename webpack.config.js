c73775b9218287111d37401e374b93ceae2a0f3c
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-18 14:25:52 -0700

    Import scatterplot example from http://nvd3.org/examples/scatter.html
    Formatting the SVG caused some errors due to wrapping some of the SVG path
    attributes.  This will be fixed when we refactor the grid lines.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './app.js',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'jsx-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    })
+  ]
+};
