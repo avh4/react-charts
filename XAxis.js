f8c4bf6264c82d2a5fa8fe4af82073a13a64f820
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-18 18:05:42 -0700

    Make XAxis and YAxis more similar

--- XAxis.js
+++ XAxis.js
@@ -7,17 +7,24 @@ var numeral = require('numeral');
 
 module.exports = React.createClass({
   render: function() {
+    var isX = true;
     var chartWidth = 605;
-    var textY = 20;
-    var dy = ".71em";
+    var chartHeight = 420;
+    var textY = isX ? 20 : undefined;
+    var textX = isX ? undefined : -10;
+    var dy = isX ? ".71em" : ".32em";
     var format = function(n) { return numeral(n).format('0.00')};
     var lines = [];
     var labels = [];
     [-10, -5, 0, 5, 10].map(function(t) {
-      var p = this.props.coord(t, 0);
+      var p = this.props.coord(t, t);
       var value = format(t);
-      lines.push(<line y2="-420" x1={p.x} x2={p.x} key={value}></line>);
-      labels.push(<text y={textY} dy={dy} x={p.x} key={value}>{value}</text>);
+      if (isX) {
+        lines.push(<line y2={-chartHeight} x1={p.x} x2={p.x} key={value}></line>);
+      } else {
+        lines.push(<line x2={chartWidth} y1={p.y} y2={p.y} key={value}></line>);
+      }
+      labels.push(<text x={textX || p.x} dy={dy} y={textY || p.y} key={value}>{value}</text>);
     }, this);
 
     return <g className="chart-axis x" transform="translate(0,420)">
@@ -26,6 +33,6 @@ module.exports = React.createClass({
       {labels}
       <text text-anchor="middle" y="36" x={chartWidth/2}>X Value</text>
       <text dy={dy} y={textY}>{format(this.props.min)}</text>
-      <text x={chartWidth} dy={dy} y={textY}>{format(this.props.max)}</text>
+      <text x={textX || chartWidth} dy={dy} y={textY || 0}>{format(this.props.max)}</text>
     </g>;
 }});
--- YAxis.js
+++ YAxis.js
@@ -7,18 +7,24 @@ var numeral = require('numeral');
 
 module.exports = React.createClass({
   render: function() {
+    var isX = false;
     var chartWidth = 605;
     var chartHeight = 420;
-    var textX = -10;
-    var dy = ".32em";
+    var textY = isX ? 20 : undefined;
+    var textX = isX ? undefined : -10;
+    var dy = isX ? ".71em" : ".32em";
     var format = function(n) { return numeral(n).format('0.00')};
     var lines = [];
     var labels = [];
     [-10, -5, 0, 5, 10].map(function(t) {
-      var p = this.props.coord(0, t);
+      var p = this.props.coord(t, t);
       var value = format(t);
-      lines.push(<line x2={chartWidth} y1={p.y} y2={p.y} key={value}></line>);
-      labels.push(<text x={textX} dy={dy} y={p.y} key={value}>{value}</text>);
+      if (isX) {
+        lines.push(<line y2={-chartHeight} x1={p.x} x2={p.x} key={value}></line>);
+      } else {
+        lines.push(<line x2={chartWidth} y1={p.y} y2={p.y} key={value}></line>);
+      }
+      labels.push(<text x={textX || p.x} dy={dy} y={textY || p.y} key={value}>{value}</text>);
     }, this);
 
     return <g className="chart-axis y">
@@ -27,6 +33,6 @@ module.exports = React.createClass({
       {labels}
       <text transform="rotate(-90)" y="-63" x={-chartHeight/2}>Y Value</text>
       <text dy={dy} y={chartHeight} x={textX}>{format(this.props.min)}</text>
-      <text x={textX} dy={dy} y="0">{format(this.props.max)}</text>
+      <text x={textX || chartWidth} dy={dy} y={textY || 0}>{format(this.props.max)}</text>
     </g>;
 }});
